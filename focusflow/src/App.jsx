68e78a762a3ab645a0a9795563ac8312a4158488
Shaheli <shahelisaha807@gmail.com>
2025-11-27 15:55:59 +0530

    Add Task

--- focusflow/src/App.jsx
+++ focusflow/src/App.jsx
@@ -4,16 +4,15 @@ import Timer from './components/Timer'
 import TaskList from './components/TaskList'
 import Stats from './components/Stats'
 import { useState } from 'react'
-
+import { useTheme } from './context/ThemeContext'
 function App() {
   const [completedFocusSessions, setCompletedFocusSessions] = useState(0)
-
+  const { currentTheme } = useTheme()
   const handleFocusComplete = () => {
     setCompletedFocusSessions(prev => prev + 1)
   }
-
   return (
-    <>
+    <div className={`app-container theme-${currentTheme}`}>
       <Navbar />
       <main>
         <div className="content-grid">
@@ -22,8 +21,7 @@ function App() {
           <Stats completedFocusSessions={completedFocusSessions} />
         </div>
       </main>
-    </>
+    </div>
   )
 }
-
 export default App
